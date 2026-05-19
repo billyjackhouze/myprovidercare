07a50f3c0bc45c3824e2921d3ea723a8cf8fce32
Billy Jack <billyjacksimpson@gmail.com>
2026-05-19 14:50:19 +0000

    config: add nshc.myprovidercare.com to CORS allowed origins

--- backend/config.py
+++ backend/config.py
@@ -35,6 +35,7 @@ class Settings(BaseSettings):
         "http://localhost:5173",
         "http://localhost:3000",
         "https://nationalcm.bjesoftware.com",
+        "https://nshc.myprovidercare.com",
     ]
 
     # Office Ally
